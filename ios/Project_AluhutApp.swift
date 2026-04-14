25d4ebcb3c82f5ead64dc8b1980e784ff8e28d03
Mario Casotti <104210853+papamario321@users.noreply.github.com>
2026-04-14 14:23:38 +0200

    Update and rename BTMessengerApp.swift to Project_AluhutApp.swift

---
+++ ios/Project_AluhutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_AluhutApp.swift
+//  Project Aluhut
+//
+//  Created by Mario Casotti on 14.04.26.
+//
+
+import SwiftUI
+
+@main
+struct Project_AluhutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
