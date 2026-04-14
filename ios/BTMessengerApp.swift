a76da88171abfc4f834953f07683f9c8fce35a87
Mario Casotti <104210853+papamario321@users.noreply.github.com>
2026-04-14 14:11:13 +0200

    Create BTMessengerApp.swift

---
+++ ios/BTMessengerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct BTMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
